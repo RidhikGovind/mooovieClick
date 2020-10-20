9217f5e3db3f5dba148bf2bcf8ac80914ccbe69c
RidhikGovind <62758905+RidhikGovind@users.noreply.github.com>
2020-10-20 19:49:52 +0530

    Updated UI

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,7 +4,7 @@ export default function SearchBar() {
   //creating a query state, with its function and setting a default value of ''
   const [query, setQuery] = useState("");
   //note that the movies are stored in an 'array'
-  const [movies,setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
@@ -15,7 +15,7 @@ export default function SearchBar() {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setMovies(data.results)
+      setMovies(data.results);
     } catch (err) {
       console.log(err);
     }
@@ -23,7 +23,7 @@ export default function SearchBar() {
 
   return (
     <div className="searchBar" onSubmit={searchMovies}>
-      <h1>MooovieClick</h1>
+      <h1>| MooovieClick |</h1>
 
       <form className="form">
         <label className="label" htmlFor="search">
@@ -44,13 +44,38 @@ export default function SearchBar() {
         </button>
       </form>
 
-      <div className="movieList" >
-  
-        {movies.filter(movie => movie.poster_path).map(movie => (
-          <div class="movieCard" key={movies.id}>
-            <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt="moviePoster"/>
-          </div>
-        ))}
+      <div className="movieList">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div class="movieCard" key={movies.id}>
+              <div className="card_img">
+                <img
+                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                  alt="moviePoster"
+                />
+              </div>
+
+              <div className="card_content">
+                {/* <h2 class="language">{movie.original_language}</h2> */}
+                <h3 className="card_title">{movie.title}</h3>
+                <h4 className="card_details">{movie.overview}</h4>
+              
+                <div class="circle_content">
+                  <div class="circle popularity">
+                    <h3>{Math.floor(movie.popularity)}</h3>
+                    <h4>Popularity</h4>
+                  </div>
+                  <div class="circle vote_average">
+                  <h3>{movie.vote_average}</h3>
+                  <h4>Average Vote</h4>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
